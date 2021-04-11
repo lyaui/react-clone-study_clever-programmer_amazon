8e1f9e32356af2ee7442adfd6d433dc925dbbfd2
Iris Huang <lyaui@outloook.com>
2021-04-12 02:03:47 +0800

    set navbar & navbar style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
+
 function App() {
   return (
     <Router>
       <div className='app'>
+        <Header></Header>
         <Switch>
           <Route path='/checkout'>
             <h1>1</h1>
---
+++ src/Icons.js
@@ -0,0 +1,4 @@
+import { AiOutlineSearch as search } from 'react-icons/ai';
+import { IoBasket as basket } from 'react-icons/io5';
+
+export { search, basket };
