c1776fa1bc62cd8673e7356aba70622802f62fc9
Iris Huang <lyaui@outloook.com>
2021-04-12 00:25:10 +0800

    set react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>1</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>2</h1>
+          </Route>
+          {/* default */}
+          <Route path='/'>
+            <h1>3</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
